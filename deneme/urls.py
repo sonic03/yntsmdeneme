a61d080613a3b243de201c00d37f6a7187f3cb30
sonic03 <mrkayacik@yahoo.com>
2021-05-06 15:26:43 +0300

    hakkimizda sayfası

--- deneme/urls.py
+++ deneme/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     
     url(r'^', TemplateView.as_view(template_name='index.html'), name='index'),
+    url(r'^hakkimizda', TemplateView.as_view(template_name='hakkimizda.html'), name='hakkimizda'),
 
 ]
 
